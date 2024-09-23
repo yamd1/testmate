f9818cb235a854b33e0c37131dc8faffdef3f85a
yamd1 <w5000c2d26@gmail.com>
2024-09-24 08:03:16 +0900

    🚧 wip

--- src/main.rs
+++ src/main.rs
@@ -6,11 +6,15 @@ fn main() -> Result<(), Box<dyn Error>> {
     let parsed_input = testmate::get_args()?;
     let cwd = env::current_dir()?;
 
-    let search_target_test_dir = set_test_dir(parsed_input.test_dir);
-    let test_file_list = get_test_file_list(&cwd, search_target_test_dir);
+    const DEFAULT_SEARCH_DIR: &str = "tests";
+    let search_target_dir = match parsed_input.test_dir {
+        Some(v) => v,
+        None => DEFAULT_SEARCH_DIR.to_string(),
+    };
+    let test_file_list = get_test_file_list(&cwd, search_target_dir);
 
     let re = Regex::new(&format!(
-        r"{}(?P<test_file_suffix>_test|Test|_Test|\.spec)(?:\.)?{}$",
+        r"{}(?P<test_file_suffix>_test|Test|_Test|\.spec)(?:\.)?{}$", // TODO: パターンを指定できるように
         &parsed_input.file.file_stem().unwrap().to_string_lossy(),
         &parsed_input
             .file
@@ -49,27 +53,37 @@ fn handle_output(matching_test_file_list: Vec<PathBuf>) -> () {
     }
 }
 
-fn get_test_file_list(cwd: &PathBuf, search_target_test_dir: HashSet<String>) -> Vec<PathBuf> {
-    search_target_test_dir
-        .into_iter()
-        .flat_map(|dir_str| match fs::read_dir(dir_str) {
-            Ok(read_dir) => read_dir
-                .filter_map(|entry| entry.ok().map(|e| cwd.join(e.path())))
-                .collect::<Vec<PathBuf>>(),
-            Err(_) => Vec::new(),
-        })
-        .collect()
+fn get_recursive_test_files() -> Option<PathBuf> {
+    None
+}
+
+fn get_test_file_list(
+    cwd: &PathBuf,
+    search_target_dir: String,
+) -> Result<Vec<PathBuf>, Box<dyn Error>> {
+    match fs::read_dir(search_target_dir) {
+        Ok(read_dir) => read_dir.flat_map(|dir_entry| {
+            dir_entry.map(|v| {
+                v.path()
+                    .is_dir()
+                    .then(|| get_test_file_list(cwd, v.path().to_str().unwrap().to_string()));
+                v.path().is_file().then(|| v.path());
+            })
+        }),
+        Err(_) => Ok(Vec::<PathBuf>::new()),
+    }
+
+    // search_target_test_dir
+    //     .into_iter()
+    //     .flat_map(|dir_str| match fs::read_dir(dir_str) {
+    //         Ok(read_dir) => read_dir
+    //             .filter_map(|entry| entry.ok().map(|e| cwd.join(e.path())))
+    //             .collect::<Vec<PathBuf>>(),
+    //         Err(_) => Vec::new(),
+    //     })
+    //     .collect()
 }
 
 fn find_matching_test_file(re: &Regex, test_file: PathBuf) -> Option<PathBuf> {
     re.is_match(test_file.to_str()?).then(|| test_file)
-}
-
-fn set_test_dir(input_test_dir: TestDirectoryName) -> HashSet<String> {
-    let default_search_dir = "tests".to_string();
-    let mut search_target_dir = HashSet::new();
-    search_target_dir.insert(default_search_dir); // TODO: default値を設定できるように
-    input_test_dir.map(|v| search_target_dir.insert(v));
-
-    search_target_dir
 }
